9323d2908f5a30980806453cb0b6e9c1fddaf210
BeauBritt <106406465+BeauBritt@users.noreply.github.com>
2025-04-21 20:12:56 -0500

    Update marchMadnessTeams.js

--- src/data/marchMadnessTeams.js
+++ src/data/marchMadnessTeams.js
@@ -2,7 +2,7 @@ export const marchMadnessTeams = [
   "Auburn", "Alabama State", "Louisville", "Creighton", "Michigan", "UC San Diego", "Texas A&M", "Yale",
   "Duke", "Ole Miss", "North Carolina", "Iowa State", "Lipscomb", "Marquette", "New Mexico", "Bryant",
   "Florida", "Norfolk State", "UConn", "Oklahoma", "Memphis", "Colorado State", "Maryland", "Grand Canyon",
-  "Missouri", "Drake", "Texas Tech", "UNC Wilmington", "Kansas", "Arkansas", "Saint Johns", "Omaha",
+  "Missouri", "Drake", "Texas Tech", "UNC Wilmington", "Kansas", "Arkansas", "St. John's", "Omaha",
   "Wofford", "Tennessee", "Utah State", "UCLA", "Troy", "Kentucky", "Xavier", "Illinois",
   "High Point", "Purdue", "McNeese", "Clemson", "Georgia", "Gonzaga", "SIU Edwardsville", "Houston",
   "Alabama", "Robert Morris", "Vanderbilt", "Saint Mary's", "Montana", "Wisconsin", "VCU", "BYU",
